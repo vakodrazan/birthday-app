89e4650bce54eac5a039214155e01f0a89b262d6
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-09-14 14:59:01 +0300

    Add image in each button

--- script.js
+++ script.js
@@ -18,18 +18,18 @@ async function displayList() {
             person => `
                 <tr data-id="${person.id}">
                     <td>
-                        <img src="${person.picture}" alt="">
+                        <img class="rounded-circle" src="${person.picture}" alt="">
                     </td>
                     <td>${person.lastName} ${person.firstName}</td>
                     <td>${person.birthday}</td>
                     <td>
-                        <button class="edit" data-id="">
-                            Edit
+                        <button class="edit" data-id="${person.id}">
+                            <img class="class="w-50 p-3" src="./assets/edit-icon.jpg" alt="">
                         </button>
                     </td>
                     <td>
                         <button class="delete" data-id="${person.id}">
-                            Delete
+                            <img class="class="w-50 p-3" src="./assets/trash-icon.jpg" alt="">
                         </button>
                     </td>
                 </tr>
