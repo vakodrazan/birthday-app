d4e6929e66d34c378beaceb3dba09ad03c9a7f6d
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-09-17 14:52:40 +0300

    Use prefix for the date

--- script.js
+++ script.js
@@ -22,13 +22,24 @@ async function fetchPeople() {
             .sort((a, b) => b.birthday - a.birthday)
             .map(
                 person => {
+                    function nthDate(day) {
+                        if (day > 3 && day < 21) return "th";
+                        switch (day % 10) {
+                            case 1: return "st";
+                            case 2: return "nd";
+                            case 3: return "rd";
+                            default: return "th"; 
+                        }
+                    }
                     const date = new Date();
                     const currentDate = new Date(person.birthday);
-                    const day = currentDate.getDate();
+                    const currentDay = currentDate.getDate();
                     const month = currentDate.getMonth() + 1;
                     const year = currentDate.getFullYear();
-                    const fullDate = `${day} / ${month} / ${year}`;
-                    const personAge = date.getFullYear() - currentDate.getFullYear();
+                    const fullDate = `${currentDay} / ${month} / ${year}`;
+                    const personAge = date.getFullYear() - year;
+
+
                     return `
                         <tr data-id="${person.id}">
                             <td>
@@ -40,7 +51,7 @@ async function fetchPeople() {
                                     Turns ${personAge} on
                                     ${new Date(person.birthday)
                                         .toLocaleString("en-US", 
-                                        { day: "numeric" })}th of 
+                                        { day: "numeric" })} <sup>${nthDate(currentDay)}</sup> of 
                                     ${new Date(person.birthday)
                                         .toLocaleString("en-US", 
                                         { month: "long" })}
