a12536eb3c7f392d18debad51f59fa446cb0dc08
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-09-28 11:14:22 +0300

    Calling the input element

--- libs/elements.js
+++ libs/elements.js
@@ -1,3 +1,5 @@
 // Call the existing element from html file
 export const tbody = document.querySelector('tbody');
 export const addListBtn = document.querySelector('button.addList');
+export const searchByName = document.querySelector('input.searchName');
+export const searchByMonth = document.querySelector('select.searchMonth');
