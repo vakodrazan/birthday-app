2be3fe52bd220cf9006aabdd991fd1d7a61ad41b
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-09-28 07:52:20 +0300

    Refactor the code by export the function of the list of people

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-import {tbody, addListBtn} from './libs/elements.js';
+import { tbody, addListBtn } from './libs/elements.js';
+import { generatePeopleList } from './libs/generate.js';
 // Get the data
 const endpoint = './people.json';
 
@@ -8,88 +9,12 @@ async function fetchPeople() {
     const data = await response.json();
     let persons = data;
 
-    function generatePeopleList(people) {
-        return people
-            .sort( function(a, b) {
-                // Sort it by Month
-                return new Date (a.birthday).getMonth() - new Date (b.birthday).getMonth();
-            })
-            .map(
-                person => {
-                    // Get the suffix for the date 
-                    function nthDate(day) {
-                        if (day > 3 && day < 21) return "th";
-                        switch (day % 10) {
-                            case 1: return "st";
-                            case 2: return "nd";
-                            case 3: return "rd";
-                            default: return "th"; 
-                        }
-                    }
-                    // Get the birthday date
-                    const today = new Date();
-                    const currentDate = new Date(person.birthday);
-                    const currentDay = currentDate.getDate();
-                    const month = currentDate.getMonth();
-                    const year = currentDate.getFullYear();
-                    const fullDate = `${currentDay}${nthDate(currentDay)} / ${month + 1}/ ${year}`;
-                    const personAge = today.getFullYear() - year;
-                    const futureAge = personAge;
-
-                    // ********** Counting date ******** \\
-                    // Counting how many days left untill the person's birthday
-                    const momentYear = today.getFullYear();
-                    const birthDayDate = new Date(momentYear, month, currentDay );
-                    let oneDay=1000*60*60*24;
-                    const getTheDate = birthDayDate.getTime() - today.getTime();
-                    const dayLeft = Math.ceil(getTheDate / (oneDay));
-
-
-                    return `
-                        <tr data-id="${person.id}">
-                            <td>
-                                <img class="rounded-circle" src="${person.picture}" alt="This the picture for ${person.firstName} ${person.lastName}">
-                            </td>
-                            <td>
-                                <span>${person.lastName} ${person.firstName}</span>
-                                <p>
-                                    Turns ${futureAge} years old on the 
-                                    ${new Date(person.birthday)
-                                        .toLocaleString("en-US", 
-                                    { month: "long" })}
-                                    <time datetime="${fullDate}">
-                                        ${new Date(person.birthday)
-                                            .toLocaleString("en-US", 
-                                            { day: "numeric" })}
-                                            <sup>${nthDate(currentDay)}</sup>
-                                    </time> 
-                                </p>
-                            </td>
-                            <td><time datetime="${fullDate}">${fullDate}</time></td>
-                            <td>${dayLeft < 0 ? dayLeft * -1 + " " + "days ago" : dayLeft + " days"}
-                            </td>
-                            <td>
-                                <button class="edit" data-id="${person.id}">
-                                    <img class="class="w-50 p-3" src="./assets/edit-icon.jpg" alt="">
-                                </button>
-                            </td>
-                            <td>
-                                <button class="delete" data-id="${person.id}">
-                                    <img class="class="w-50 p-3" src="./assets/trash-icon.jpg" alt="">
-                                </button>
-                            </td>
-                        </tr>
-                    `
-                })
-            .join('');
-    }
-
     // display the list of people
     const displayList = () => {
         const html = generatePeopleList(persons);
         tbody.innerHTML = html;
     }
-    
+
     displayList();
 
     // Here are something to do with the popup
