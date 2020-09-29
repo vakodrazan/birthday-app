ce672efd65bdc604441a5994e056abf8e2a79730
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-09-29 14:46:54 +0300

    Use svg for the button's icon

--- libs/generate.js
+++ libs/generate.js
@@ -35,7 +35,7 @@ export function generatePeopleList(people) {
 
 
                 return `
-                    <tr data-id="${person.id}">
+                    <tr data-id="${person.id}"> 
                         <td>
                             <img class="rounded-circle" src="${person.picture}" alt="This the picture for ${person.firstName} ${person.lastName}">
                         </td>
@@ -60,12 +60,12 @@ export function generatePeopleList(people) {
                         </td>
                         <td>
                             <button class="edit" data-id="${person.id}">
-                                <img class="class="w-50 p-3" src="./assets/edit-icon.jpg" alt="">
+                            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>
                             </button>
                         </td>
                         <td>
                             <button class="delete" data-id="${person.id}">
-                                <img class="class="w-50 p-3" src="./assets/trash-icon.jpg" alt="">
+                            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg>
                             </button>
                         </td>
                     </tr>
