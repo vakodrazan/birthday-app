364e2d1e708ea958ea660ec853b9695a8ea37b1c
Noeline Marie Jeanne <noeline.mar@onja.org>
2021-03-16 09:14:04 +0300

    Change an element into date so the browser can read it

--- dist/script.75da7f30.js
+++ dist/script.75da7f30.js
@@ -204,9 +204,9 @@ function generatePeopleList(people) {
                             </p>
                         </div>
                         <div class="wrapper-actions">
-                            <span class="date">
+                            <time datetime="${fullDate}" class="date">
                                 ${dayLeft < 0 ? dayLeft * -1 + " " + "days ago" : dayLeft <= 1 ? dayLeft + " " + "day" : dayLeft + 'days'}
-                            </span>
+                            </time>
                             <div class="actions">
                                 <button class="edit" data-id="${person.id}">
                                     <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
